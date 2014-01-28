0d7fa43f5f78bdb3b6bbf5335437192f7c300f6a
Vad1mo <bauer.vadim@gmail.com>
2014-01-29 00:16:57 +0100

    Reverting back to use assembly. Change project packaging to pom. Track events for analytics
    changed site content added proper links

--- src/main/website/assets/js/konik.js
+++ src/main/website/assets/js/konik.js
@@ -1,20 +1,4 @@
 
-// tree collapse
-/*
-$(function () {
-    $('.tree li:has(ul)').addClass('parent_li').find(' > p').attr('title', 'Collapse this branch');
-    $('.tree li.parent_li > p').on('click', function (e) {
-        var children = $(this).parent('li.parent_li').find(' div.ulist > ul > li');
-        if (children.is(":visible")) {
-            children.hide('fast');
-            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-        } else {
-            children.show('fast');
-            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-        }
-        e.stopPropagation();
-    });
+$('#download-zip-package').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'download-zip-package');
 });
-*/
-
-hljs.initHighlightingOnLoad();
