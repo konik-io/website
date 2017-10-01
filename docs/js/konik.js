320059549f82a660bef80d97337da98fe47231df
Vadim Bauer <bauer.vadim@gmail.com>
2017-10-02 01:03:09 +0200

    switch to docs

---
+++ docs/js/konik.js
@@ -0,0 +1,16 @@
+//Highlight js init 
+hljs.configure({
+  tabReplace: '    ', // 4 spaces
+});
+
+hljs.initHighlightingOnLoad();
+
+$('.download-zip-package').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'download-zip-package');
+});
+
+$(".navbar-fixed-top").headroom({
+	  "tolerance": 5,
+	  "offset": 100});
+
+$(function () {$("[data-toggle='tooltip']").tooltip();});
