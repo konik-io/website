f7b9d4b839144e59b56ba999bcef334bc2057f01
Vadim Bauer <1492007+Vad1mo@users.noreply.github.com>
2019-03-22 13:34:04 +0100

    switch to doo update legal notice

---
+++ do.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+# Do - The Simplest Build Tool on Earth.
+# Documentation and examples see https://github.com/8gears/do
+
+set -e -u # -e "Automatic exit from bash shell script on error"  -u "Treat unset variables and parameters as errors"
+
+build() {
+   #run the website locally
+    mvn -q package
+
+}
+
+run() {
+   mvn -q exec:java
+}
+
+all() {
+   build
+   run
+}
+
+"$@" # <- execute the task
+
+[ "$#" -gt 0 ] || printf "Usage:\n\t./do.sh %s\n" "($(compgen -A function | grep '^[^_]' | paste -sd '|' -))"
+
+
+
+
+
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-#run the website locally
-mvn -q package
-mvn -q exec:java 
